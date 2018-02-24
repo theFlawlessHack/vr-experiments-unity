f0edfb06519ac370f5f8d3e0048ccfb30fd63c6b
Jessica Joseph <jessjeanjo@gmail.com>
2018-02-24 07:27:03 -0500

    random cubes generated by code

---
+++ ExploringUnityCode/Assets/Cube.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+
+    public float sizeModifier = .5f;
+    public string newName = "Cubie ";
+    public bool isRotated = false;
+
+    void Start() {
+        transform.name = ImproveName(newName);
+
+        if (isRotated) {
+            Debug.Log("Is rotated");
+            transform.localEulerAngles = Vector3.one * 45;
+        }
+    }   
+
+    void Update() {
+        transform.localScale = Vector3.one * sizeModifier;
+    }
+
+    string ImproveName(string name) {
+        return "~~[" + name + "]~~";
+    }
+}
---
+++ ExploringUnityCode/Assets/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject cubePrefab;
+    public int cubeAmount = 10;
+    public float randomArea = 5f;
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < cubeAmount; i++) {
+            GameObject cubeObject = Instantiate(cubePrefab);
+            cubeObject.transform.position = new Vector3(
+                Random.Range(-randomArea, randomArea),
+                Random.Range(-randomArea, randomArea),
+                0);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
