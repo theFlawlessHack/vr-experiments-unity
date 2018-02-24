eb6855f3b00a75ce7e87516514a2cdc630d7cc26
Jessica Joseph <jessjeanjo@gmail.com>
2018-02-24 07:29:16 -0500

    cha cha cube, moving a cube using input

---
+++ ExploringUnityInput/Assets/GameController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject[] gameCameras;
+
+    private int gameCameraIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+        FocusOnCamera(gameCameraIndex);
+ 
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            ChangeCamera(1);
+        }
+
+        if (Input.GetMouseButtonDown(1)) {
+            ChangeCamera(-1);
+        }
+	}
+
+    void FocusOnCamera(int index) {
+        for (int i = 0; i < gameCameras.Length; i++) {
+            gameCameras[i].SetActive(i == index);
+        }
+    }
+
+    void ChangeCamera(int direction) {
+        gameCameraIndex += direction;
+
+        if (gameCameraIndex >= gameCameras.Length){
+            gameCameraIndex = 0;
+        }
+
+        if (gameCameraIndex < 0) {
+            gameCameraIndex = gameCameras.Length - 1;
+        }
+
+        FocusOnCamera(gameCameraIndex);
+    }
+}
---
+++ ExploringUnityInput/Assets/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 3.5f;
+    public float jumpingForce = 250f;
+    public float rotatingSpeed = 100f;
+
+    private bool canJump = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("right")) {
+            transform.RotateAround(transform.position, Vector3.up, rotatingSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("left")) {
+            transform.RotateAround(transform.position, Vector3.up, -rotatingSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown("space") && canJump) {
+            canJump = false;
+            GetComponent<Rigidbody>().AddForce(0, jumpingForce, 0);
+        }
+
+        if (Input.GetKey("up"))
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("down"))
+        {
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+
+
+        if (Input.GetKey("left") && Input.GetKey("up")) {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("right") && Input.GetKey("down"))
+        {
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.name == "Floor")
+        {
+            canJump = true;
+        }
+    }
+}
