ff93e89f5a7c41874d2d33f9d92c07b00f08c83c
Jessica Joseph <jessjeanjo@gmail.com>
2018-02-24 07:36:35 -0500

    vr headset compat. 👀 @ button and open the castle door

---
+++ ExploringVRInteractions/Assets/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position,transform.forward, out hit)) {
+            if (hit.transform.GetComponent<DoorButton>() != null) {
+                hit.transform.GetComponent<DoorButton>().OnLook();
+            }
+        }
+	}
+}
